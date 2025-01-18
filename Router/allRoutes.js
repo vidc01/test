9ec07e64baabddf57a1bdd5b500490685aa7c468
vidc01 <victor.dct01@gmail.com>
2025-01-18 17:33:38 +0100

    ajout routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,10 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes et ajoutez dossier "pages" et dedans un dossier "home.html" comme définit ici dans ce tableau
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    //new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
