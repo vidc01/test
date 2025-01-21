e518fd1d3b46118faba420149312457e3dce12fa
vidc01 <victor.dct01@gmail.com>
2025-01-21 17:45:29 +0100

    Galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes et ajoutez dossier "pages" et dedans un dossier "home.html" comme définit ici dans ce tableau
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    //new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
