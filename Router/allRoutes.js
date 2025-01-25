b69fc74f673f7168ef38360613ea463101f593fe
vidc01 <victor.dct01@gmail.com>
2025-01-25 18:06:51 +0100

    ajout regex + btn d'inscription disabled

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
 ];
