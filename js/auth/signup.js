2423b372c98ee68341207f16c80ada2c9e217148
vidc01 <victor.dct01@gmail.com>
2025-01-26 12:31:47 +0100

    ajout feedback sur champs d'inscription

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -20,13 +20,45 @@ function validateForm(){
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPreNom);
     const mailOk = validateMail(inputMail);
-    if(nomOk && prenomOk && mailOk){
+    const passwordOk = validatePassword(inputPassword);
+    const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidationPassword);
+    if(nomOk && prenomOk && mailOk && passwordOk && passwordConfirmOk){
         btnValidation.disabled = false;
     }
     else{
         btnValidation.disabled = true;
     }
 }
+function validateConfirmationPassword(inputPwd, inputConfirmPwd){
+    if(inputPwd.value == inputConfirmPwd.value){
+        inputConfirmPwd.classList.add("is-valid");
+        inputConfirmPwd.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        inputConfirmPwd.classList.add("is-invalid");
+        inputConfirmPwd.classList.remove("is-valid");
+        return false;
+    }
+}
+
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+}
+
 
 function validateMail(input){
         //Définir mon regex
