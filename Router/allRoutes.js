5147a02106137fed2c42ac1336b6ec660f0a1b53
vidc01 <victor.dct01@gmail.com>
2025-01-28 16:20:55 +0100

    ajout modals galerie et reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
