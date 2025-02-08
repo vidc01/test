de7a73a1bd87d1fda73c0a1916c13d750bf285fc
vidc01 <victor.dct01@gmail.com>
2025-02-08 17:52:25 +0100

    add cookies et permissions de routes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,15 +1,17 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes et ajoutez dossier "pages" et dedans un dossier "home.html" comme définit ici dans ce tableau
+//Définir ici vos routes
+
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "Galerie", "/pages/galerie.html"),
-    new Route("/signin", "Connexion", "pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "pages/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "pages/auth/account.html"),
-    new Route("/editPassword", "Changement de mot de passe", "pages/auth/editPassword.html"),
-    new Route("/allResa", "Vos réservations", "pages/reservations/allResa.html"),
-    new Route("/reserver", "Réserver", "pages/reservations/reserver.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/galerie.js"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["client", "admin"]),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html", ["client"]),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html", ["client"]),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
---
+++ js/auth/signin.js
@@ -0,0 +1,28 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSingin = document.getElementById("btnSignin");
+
+
+btnSingin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+    
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        //Il faudra récupérer le vrai token
+        const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf";
+        setToken(token);
+        //placer ce token en cookie
+
+        setCookie(RoleCookieName, "admin", 7);
+        
+        setTimeout(function(){
+            window.location.replace("/");
+        }, 0);
+        
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
