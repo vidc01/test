c552b5c21f93f66f65db567072ef859ded06ee99
vidc01 <victor.dct01@gmail.com>
2025-02-15 18:58:21 +0100

    faille xss function sanitize

--- js/script.js
+++ js/script.js
@@ -24,9 +24,9 @@ function getToken(){
 }
 
 function setCookie(name,value,days) {
-    var expires = "";
+    let expires = "";
     if (days) {
-        var date = new Date();
+        let date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
@@ -34,12 +34,12 @@ function setCookie(name,value,days) {
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(const element of ca) {
+        let c = element;
+        while (c.startsWith(' ')) c = c.substring(1,c.length);
+        if (c.startsWith(nameEQ)) return c.substring(nameEQ.length,c.length);
     }
     return null;
 }
@@ -55,8 +55,6 @@ function isConnected(){
         return true;
     }
 }
-
-
 
 /*
 disconnected 
@@ -95,4 +93,13 @@ function showAndHideElementsForRoles(){
                 break;
         }
     })
+}
+function sanitizeHtml(text){
+    // Créez un élément HTML temporaire de type "div"
+    const tempHtml = document.createElement('div');
+    // Affectez le texte reçu en tant que contenu texte de l'élément "tempHtml"
+    tempHtml.textContent = text;
+    // Utilisez .innerHTML pour récupérer le contenu de "tempHtml"
+    // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
+    return tempHtml.innerHTML;
 }
